beb3620699e443fdbc4a3b56266e8dcfb84a1ef3
Atharv Sathe <atharvsathe28704@gmail.com>
2023-10-20 02:13:52 +0530

    Added IP to localhost
    Now any person on the same wi-fi as me can upload files using his device on the server on my local device.

--- Qraze/Upload/server.js
+++ Qraze/Upload/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const multer = require('multer');
 
 const app = express();
+const port = 3000;
+
 const path = require('path');
 
 const fileStorageEngine = multer.diskStorage({
@@ -33,4 +35,4 @@ app.post('/multiple', upload.array('spreadsheets', 3), (req, res) => {
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, '172.25.247.30' , () => console.log('Server running on port 3000'));
